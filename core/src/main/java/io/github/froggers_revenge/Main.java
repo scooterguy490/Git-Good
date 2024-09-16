24668a8e7399bcc8d0431ac6620453c9f26ae6f0
MatthewS2027 <mattlsherman@icloud.com>
2024-09-16 14:23:12 -0500

    Update Main.java
    deleted test int

--- core/src/main/java/io/github/froggers_revenge/Main.java
+++ core/src/main/java/io/github/froggers_revenge/Main.java
@@ -29,7 +29,7 @@ public class Main extends ApplicationAdapter {
         batch.begin();
         batch.draw(image, 140, 210); //image size
         batch.end();
-        int number = 10;
+        
     }
 
     @Override
