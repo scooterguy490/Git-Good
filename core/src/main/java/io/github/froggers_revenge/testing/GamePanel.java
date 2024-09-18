d351aae0bdfdd0293f691906a2c9d6c2517dc5c1
MatthewS2027 <155858054+MatthewS2027@users.noreply.github.com>
2024-09-18 15:00:09 -0500

    Window size
    Changed to original frogger resolution but scaled up by 3.5x. This is because the original resolution would be a very small window.

--- core/src/main/java/io/github/froggers_revenge/testing/GamePanel.java
+++ core/src/main/java/io/github/froggers_revenge/testing/GamePanel.java
@@ -6,21 +6,10 @@ import javax.swing.JPanel;
 
 public class GamePanel extends JPanel{
 
-    //This was from a youtube video by RyiSnow. Im not sure what we want the size of the tiles to be or how to make it
-    //fit with 1016x1024 so I am commenting this portion out. 
-    /* 
-    //Screen Settings
-    final int originalTileSize = 16;    // 16x16 tile
-    final int scale = 3;    
-
-    final int tileSize = originalTileSize * scale;  
-    final int maxScreenCol = 14;
-    final int maxScreenRow = 14;
-    final int screenWidth = tileSize * maxScreenCol;    //672 pixels
-    final int screenHeight = tileSize * maxScreenRow;   //672 pixels
-    */
-    final int screenWidth = 1016;
-    final int screenHeight = 1024;
+    //original frogger resolution but scaled up by 3.5x
+    final int screenWidth = 784;
+    final int screenHeight = 896;
+    
     
 
     public GamePanel() {
@@ -28,6 +17,7 @@ public class GamePanel extends JPanel{
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);   //can improve rendering performance
+        
         
 
     }
